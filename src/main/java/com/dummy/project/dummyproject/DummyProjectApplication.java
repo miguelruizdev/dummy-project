400a0ec0ad0dfd4a0413e1da68e96ca7740e6b3f
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2018-09-17 11:27:59 +0200

    Draft create

---
+++ src/main/java/com/dummy/project/dummyproject/DummyProjectApplication.java
@@ -0,0 +1,12 @@
+package com.dummy.project.dummyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyProjectApplication.class, args);
+	}
+}
